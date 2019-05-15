e888d01529f33f1c4b01c2490859c29cb5de6a44
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2019-05-15 19:27:44 +0700

    CabangSeeder udah bisa

--- app/Models/Kelurahan.php
+++ app/Models/Kelurahan.php
@@ -8,6 +8,7 @@ class Kelurahan extends Model
 {
     protected $table = 'kelurahan';
     protected $fillable = ['kecamatan_id', 'nama_kelurahan'];
+    public $incrementing = false;
 
     public function kecamatan(){
         return $this->belongsTo('App\Models\Kecamatan');
--- database/seeds/CabangSeeder.php
+++ database/seeds/CabangSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('memory_limit', '512M');
+
 use Illuminate\Database\Seeder;
 use Faker\Provider\id_ID\Address as Faker;
 use Illuminate\Support\Facades\DB;
@@ -17,14 +19,13 @@ class CabangSeeder extends Seeder
     {
         //
         // $faker = Faker::create();
-        $data = Kelurahan::all();
-        for ($i = 0; $i < 2000; $i++){
-            $newId = $data->random()->id;
-            $namaKelurahan = Kelurahan::select('nama_kelurahan')->where('id', $newId)->get();
-            $nama_cabang = $namaKelurahan[0]['nama_kelurahan'];
+        $data = Kelurahan::all()->pluck('id');
+        for ($i = 0; $i < 5000; $i++){
+            $newId = $data->random(1);
+            $namaKelurahan = Kelurahan::select('nama_kelurahan')->where('id', $newId[0])->get();
             DB::table('cabang')->insert([
-                'kelurahan_id' => $newId,
-                'nama_cabang' => $nama_cabang,
+                'kelurahan_id' => $newId[0],
+                'nama_cabang' => "JNE ".$namaKelurahan[0]['nama_kelurahan'],
                 'alamat_cabang' => Faker::streetPrefix().' '.Faker::street().' No. '.(string)Faker::buildingNumber(),
             ]);
         }
