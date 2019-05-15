1ea33c115082e66ae37adaf9533a408cca2b65ed
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2019-05-15 22:23:32 +0700

    KurirSeeder sudah

--- database/seeds/KurirSeeder.php
+++ database/seeds/KurirSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('memory_limit', '512M');
+
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
@@ -17,12 +19,15 @@ class KurirSeeder extends Seeder
         //
 
         $faker = Faker::create('id_ID');
-        for ($i = 0; $i < 5000; $i++){
+        for ($i = 0; $i < 30000; $i++){
             DB::table('kurir')->insert([
                 'nama_kurir' => $faker->name,
-                'no_hp_kurir' => $faker->phoneNumber,
-                'username' => Str::random(10),
+                'no_hp_kurir' => "081".mt_rand(000000000, 999999999),
+                'username' => $faker->userName,
                 'password' => bcrypt(Str::random(10)),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
         }
     }
